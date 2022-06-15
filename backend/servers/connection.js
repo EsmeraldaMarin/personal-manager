b55e0fc565cc122db42ecb464a5c55df48bb78d6
Esmeralda Marín <65983756+EsmeraldaMarin@users.noreply.github.com>
2022-06-14 22:25:32 -0300

    Update connection.js

--- backend/servers/connection.js
+++ backend/servers/connection.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
     host: 'localhost',
     port: '3306',
     user: 'root',
-    password: '45092611esmeM03',
+    password: 'xxx',
     database: 'personal_manager'
 })
 
